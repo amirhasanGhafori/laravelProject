f37c98133b2b029d4957e7378890e89681334810
amirhasan <amirhasandidloo@gmail.com>
2020-09-27 11:04:32 -0700

    =create

---
+++ app/Models/Thread.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Thread extends Model
+{
+    use HasFactory;
+
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+
+    public function channel(){
+        return $this->belongsTo(Channel::class);
+    }
+
+    public function answers(){
+        return $this->hasMany(Answers::class);
+    }
+
+}
